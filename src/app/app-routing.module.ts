2e30b3b555967e7ea8f9cf3da230e63db2148724
pbout <pavlos.boudagidis@gmail.com>
2019-05-14 01:03:40 +0100

    Angular initialization. * Added routes for landing, auth, home * Added components for login and registration forms (Work in progress)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from "./components/landing/landing.component";
+import {AuthComponent} from "./components/auth/auth.component";
+import {HomeComponent} from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { PagesComponent } from './components/pages.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/auth/registration/registration.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagesComponent
+    PagesComponent,
+    LandingComponent,
+    AuthComponent,
+    HomeComponent,
+    RegistrationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
